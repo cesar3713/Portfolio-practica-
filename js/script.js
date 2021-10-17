9a26e39e133cf0a93e91c03e022313a1e1e716dc
Cesar <cesar3713@outlook.com>
2021-10-17 16:03:00 -0300

    detalles visuales

--- js/script.js
+++ js/script.js
@@ -2,12 +2,16 @@
 import hamburgerMenu from "./hamburger-menu.js";
 import contactFormValidations from "./validaciones-formulario.js";
 
+
 const d = document;
 
 d.addEventListener("DOMContentLoaded", e => {
     hamburgerMenu(".panel-btn",".nav","ul li a");
     contactFormValidations();
+    
 })
+
+
 
 
 
