41deb75a6b69c5739a520385ddfcede3b3ac1ecb
Cesar <cesar3713@outlook.com>
2021-10-20 01:45:20 -0300

    detalles form input label

--- js/script.js
+++ js/script.js
@@ -9,6 +9,37 @@ d.addEventListener("DOMContentLoaded", e => {
     hamburgerMenu(".panel-btn",".nav","ul li a");
     contactFormValidations();
     
+    let a = function(){
+
+		$("#name").val("");
+
+		$("#name").focusout(function(){
+			if($(this).val() != ""){
+				$(this).addClass("focus-label-name");
+			}else{
+				$(this).removeClass("focus-label-name");
+			}
+		})
+	};
+
+    a();
+
+    let b = function(){
+
+		$("#email").val("");
+
+		$("#email").focusout(function(){
+			if($(this).val() != ""){
+				$(this).addClass("focus-label-email");
+			}else{
+				$(this).removeClass("focus-label-email");
+			}
+		})
+	};
+
+    b();
+
+
 })
 
 
