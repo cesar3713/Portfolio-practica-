88ae41538128d309fec54e4877373037aa6f5267
Cesar <cesar3713@outlook.com>
2021-11-16 12:05:45 -0300

    scroll reveal detalles

--- js/scroll-animations-mando.js
+++ js/scroll-animations-mando.js
@@ -1,53 +1,54 @@
 window.sr = ScrollReveal();
 
 const opac = 0; 
-const time = 4000;
+const time = 3000;
+const dist = "70px";
 
     sr.reveal("#Photo_and_data",{
         duration: time,
         origin: "bottom",
-        distance: "100px",
+        distance: dist,
         opacity: opac
     });
 
     sr.reveal("header",{
         duration: time,
         origin: "bottom",
-        distance: "-100px",
+        distance: "-70px",
         opacity: opac
     });
 
     sr.reveal("#h3-proyects-title",{
         duration: time,
         origin: "bottom",
-        distance: "100px",
+        distance: dist,
         opacity: opac
     });
 
     sr.reveal("#proyect",{
         duration: time,
         origin: "bottom",
-        distance: "100px",
+        distance: dist,
         opacity: opac
     });
 
     sr.reveal(".skills",{
         duration: time,
         origin: "bottom",
-        distance: "100px",
+        distance: dist,
         opacity: opac
     });
 
     sr.reveal("#contact-icons",{
         duration: time,
         origin: "bottom",
-        distance: "100px",
+        distance: dist,
         opacity: opac
     });
 
     sr.reveal("#box-form",{
         duration: time,
         origin: "bottom",
-        distance: "100px",
+        distance: dist,
         opacity: opac
     });
