781cc06db7732fa87417c72d81d44cea513263fa
Cesar <cesar3713@outlook.com>
2021-11-16 11:32:44 -0300

    scrollreveal

---
+++ js/scroll-animations-mando.js
@@ -0,0 +1,53 @@
+window.sr = ScrollReveal();
+
+const opac = 0; 
+const time = 4000;
+
+    sr.reveal("#Photo_and_data",{
+        duration: time,
+        origin: "bottom",
+        distance: "100px",
+        opacity: opac
+    });
+
+    sr.reveal("header",{
+        duration: time,
+        origin: "bottom",
+        distance: "-100px",
+        opacity: opac
+    });
+
+    sr.reveal("#h3-proyects-title",{
+        duration: time,
+        origin: "bottom",
+        distance: "100px",
+        opacity: opac
+    });
+
+    sr.reveal("#proyect",{
+        duration: time,
+        origin: "bottom",
+        distance: "100px",
+        opacity: opac
+    });
+
+    sr.reveal(".skills",{
+        duration: time,
+        origin: "bottom",
+        distance: "100px",
+        opacity: opac
+    });
+
+    sr.reveal("#contact-icons",{
+        duration: time,
+        origin: "bottom",
+        distance: "100px",
+        opacity: opac
+    });
+
+    sr.reveal("#box-form",{
+        duration: time,
+        origin: "bottom",
+        distance: "100px",
+        opacity: opac
+    });
